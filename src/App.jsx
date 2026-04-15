4e0174cd426b496a449fec101cfcc92286c5a278
abubakkarsiddik123 <abu089604@gmail.com>
2026-04-15 13:05:11 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h2 className='text-3xl text-red-500 font-bold text-center'>Hello react</h2>
+    </>
+  )
+}
+
+export default App
